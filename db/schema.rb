c766aabe75daba2db42f896523f26599234a7a1b
Alki Delichatsios <alkidel@gmail.com>
2019-02-25 16:30:42 +0100

    added decision model and database

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_25_141801) do
+ActiveRecord::Schema.define(version: 2019_02_25_152813) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "decisions", force: :cascade do |t|
+    t.string "title"
+    t.string "category"
+    t.text "description"
+    t.integer "result"
+    t.text "minutes"
+    t.date "council_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,6 +34,9 @@ ActiveRecord::Schema.define(version: 2019_02_25_141801) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.boolean "is_town_admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
