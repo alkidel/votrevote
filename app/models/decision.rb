a1de9a577f820c3944a0509a9fce2c9047e6006a
Alki Delichatsios <alkidel@gmail.com>
2019-02-25 17:07:09 +0100

    created validations

--- app/models/decision.rb
+++ app/models/decision.rb
@@ -1,2 +1,9 @@
 class Decision < ApplicationRecord
+  has_many :votes
+  has_many :users, through: :votes
+
+  validates :title, presence: true
+  validates :category, presence: true
+  validates :council_date, presence: true
+  validates :result, exclusion: { in: [nil] }
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,8 @@
 class Vote < ApplicationRecord
-  belongs_to :user
-  belongs_to :decision
+  belongs_to :users
+  belongs_to :decision, dependent: :destroy
+
+  validates :user, presence: true
+  validates :decision, presence: true
+  validates :result, presence: true
 end
