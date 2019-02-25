4e0b18155135dabe78a8f4943994901c2a010e5a
Alki Delichatsios <alkidel@gmail.com>
2019-02-25 17:48:54 +0100

    added default to result

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_25_154026) do
+ActiveRecord::Schema.define(version: 2019_02_25_164538) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2019_02_25_154026) do
     t.string "title"
     t.string "category"
     t.text "description"
-    t.integer "result"
+    t.integer "result", default: 1
     t.text "minutes"
     t.date "council_date"
     t.datetime "created_at", null: false
