de3a402664ae6f7e6fbf899b762879a193b3a948
Pierre Lestang <plestang@gmail.com>
2019-02-26 11:44:01 +0100

    routes created

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :decisions do
+    resources :votes, only: [:create, :new, :edit, :update, :destroy]
+  end
+  resources :town, only: [:create, :new]
+  resources :users, only: [:destroy, :edit, :update, :show]
 end
