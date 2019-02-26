031ad85601fcf34cf55090f3bd3dde72a3996d2a
Alki Delichatsios <alkidel@gmail.com>
2019-02-26 18:53:30 +0100

    decision cards

--- app/controllers/decisions_controller.rb
+++ app/controllers/decisions_controller.rb
@@ -3,6 +3,7 @@ class DecisionsController < ApplicationController
 
   def index
     @decisions = Decision.all
+    @future_decisions = Decision.where("council_date > ?", DateTime.now)
       # @category = params[:query]
       # @address = params[:address]
       # if @address.present? && @category.present?
