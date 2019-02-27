2885f3f4baa437a449be393d10e63ff4642b0b70
Alki Delichatsios <alkidel@gmail.com>
2019-02-27 15:26:41 +0100

    fixes

--- app/models/decision.rb
+++ app/models/decision.rb
@@ -1,9 +1,11 @@
 class Decision < ApplicationRecord
   # belongs_to :town
-  has_many :votes
+  has_many :votes, dependent: :destroy
   has_many :users, through: :votes
 
   mount_uploader :photo, PhotoUploader
+
+  # before_destroy :delete_photo
 
   enum result: %i[pending accepted rejected deferred]
 
@@ -11,4 +13,8 @@ class Decision < ApplicationRecord
   validates :category, presence: true
   validates :council_date, presence: true
   validates :result, presence: true
+
+  def delete_photo
+    photo.file.delete
+  end
 end
