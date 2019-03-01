d510415206d0169f82d31bca2552df9519354650
Pierre Lestang <plestang@gmail.com>
2019-03-01 15:34:56 +0100

    recup

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,11 @@
+class UserMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.next_agenda.subject
+  #
+  def next_agenda
+    mail to: "to@example.org"
+  end
+end
---
+++ test/mailers/previews/user_mailer_preview.rb
@@ -0,0 +1,9 @@
+# Preview all emails at http://localhost:3000/rails/mailers/user_mailer
+class UserMailerPreview < ActionMailer::Preview
+
+  # Preview this email at http://localhost:3000/rails/mailers/user_mailer/next_agenda
+  def next_agenda
+    UserMailer.next_agenda
+  end
+
+end
